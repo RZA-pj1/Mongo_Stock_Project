0b0d017e56def3bda846605328b162fcb36d7a47
R.Z.A <53036090+GunHeeKim127@users.noreply.github.com>
2022-09-30 14:07:16 +0900

    0930최신파일

--- myapp/routes/userManagement.js
+++ myapp/routes/userManagement.js
@@ -1,106 +1,64 @@
-/**********************************************
+/**************************************************
  * 담당자 : 김건희
- * 페이지 : 관리자 페이지
- * 기능   : 유저 추방 및 권한 부여등 관리자의
- *          역할을 수행할 수 있게 만들어주는 기능 
- **********************************************/
-
+ * 페이지 : 유저관리 페이지
+ * 기능   : 관리자만 이용가능
+ *          이용자의 권한부여
+ *          해당유저 추방가능
+ *          현재 사이트에 가입한 유저 리스트 확인가능
+ **************************************************/
 var express = require('express');
 var router = express.Router();
 
-var { User } = require('../Models/addUser');
-var { auth } = require('../Middleware/Auth');
-var { Stock } = require('../Models/stockName');
-var { Category } = require('../Models/category');
+const { User } = require('../Models/addUser')
+const { auth } = require('../Middleware/Auth')
 
-
-router.post('/userManagement',(req,res)=>{
-  return res.json({
-    message:"success"
+//관리자 페이지 띄우기
+router.get('/', auth, (req, res) => {
+  //현재 로그인 중인 유저의 정보중 관리자 권한 정보와 이름을 가져오기  
+  const users = User({
+    userName: req.user.userName,
+    role: req.user.role
   })
-})
-router.get('/userManagement',auth,(req, res)=>{
-  const users=User({userName:req.user.userName})
-  const use =User({
-    userName    : req.params.userName,
-    teamPosition: req.params.teamPosition,
-    email       : req.params.email,
-    userId      : req.user.userId,
-    role        : req.user.role
-  }) 
-    User.findOne({ 
-      userId    : req.params.userId,
-      role      :req.params.role
-     },(err,user) => { 
-      if (use.role==1) {
-        console.log("Read Detail 완료");
-        console.log(user)
-          User.updateOne({
-            userId:req.body.userId
-          },
-          {
-            rentalMan : req.body.rentalMan,
-            registMan : req.body.registMan,
-            editMan   : req.body.editMan
-          })
-          return res.render('userManagement',{
-            userId    : req.user.userId,
-            _id       : req.user._id,
-            isAdmin   : req.user.role === 0 ? false : true,
-            isAuth    : true,
-            email     : req.user.email,
-            userName  : req.user.userName,
-            teamList  : req.user.teamList,
-            role      : req.user.role,
-            teamPosition :  req.user.teamPosition,
-            token     : req.user.token,
-            isEdit    :req.user.editMan === 0 ? false:true,
-            isRental  :req.user.rentalMan === 0 ? false:true,
-            isRegist  : req.user.registMan ===0 ? false:true,
-            editMan   : req.params.editMan,
-            rentalMan : req.params.rentalMan,
-            registMan : req.paramsregistMan,
-            hellow   : `${users.userName}`+"관리자님 환영합니다.",
-            layout    : "./userManagement"
-          })}
-        else return res.json({message:"권한이 없습니다"})
+  //관리자인지 아닌지 확인
+  if (users.role == 1) {
+    //데이터베이스에 들어있는 모든 유저정보 가져오기
+    User.find({}).then(user => {
+      console.log("Read Detail 완료");
+      console.log(user)
+      return res.render('userManagement', {
+        user:user,
+        hellow: `${users.userName}관리자님 환영합니다.`,
       })
+    })
+  }
+  else return res.json({ message: "권한이 없습니다" })
 })
-/********************************************
- * 관리자 권한 부여  
- ********************************************/
-router.post('/admin/:userId',auth,(req,res)=>{
+router.post('/userManagement/authority', auth, (req, res) => {
   const user = new User({
     isAdmin: req.body.role === 0 ? false : true,
     isAuth: true,
-    isEdit:req.user.editMan === 0 ? false:true,
-    isRental:req.user.rentalMan === 0 ? false:true,
-    isRegist : req.user.registMan ===0 ? false:true,
-    email: req.body.email,
-    userId:req.body.userId,
-    userName: req.body.userName,
-    teamList: req.body.teamList,
-    role: req.body.role,
-    teamPosition :req.body.teamPosition,
-    token: req.body.token,
-    editMan : req.body.editMan,
-    rentalMan : req.body.rentalMan,
-    registMan : req.body.registMan
+    isEdit: req.user.editMan === 0 ? false : true,
+    isRental: req.user.rentalMan === 0 ? false : true,
+    isRegist: req.user.registMan === 0 ? false : true,
+    editMan: req.body.editMan,
+    rentalMan: req.body.rentalMan,
+    registMan: req.body.registMan
   })
-  //비밀번호 초기화
-  //client비밀번호 초기화->사번확인->사번에 저장된 비밀번호 초기화
-  if(req.user.userId===''){
+  //권한부여
+  //라디오 박스 클릭 후 권한부여 클릭하면 권한 부여 혹은 권한 제거
+  if (req.user.userId === '') {
     res.status(400).send("userName required");
   }
   const userId = req.body.userId;
-  User.findOne({ userNumber: userId} ,(err,user)=>{
-      if (!user) return res.status(404).json({ message: "post not found" });
-      console.log("Read Detail 완료");
-      return res.status(200).json({
-        message: "Read Detail success",
-        data: {user: {user}},
-        message:`${user.userName}`+"관리자님 환영합니다."
-      });
+  User.findOneAndUpdate({ userId: userId }, (err, user) => {
+    
+    if (!user) return res.status(404).json({ message: "post not found" });
+    console.log("Read Detail 완료");
+    return res.status(200).json({
+      message: "Read Detail success",
+      data: { user: { user } },
+      message: `${user.userName}관리자님 환영합니다.`
+    });
   })
 })
 
